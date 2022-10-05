1644eb15dc6ccfa9876f8dc5fd3d8a486d227279
deividrx <galdinodeivid@gmail.com>
2022-10-05 15:07:59 -0300

    add description to OpenApi config

--- src/main/java/br/com/senai/propetservice/config/OpenApiConfig.java
+++ src/main/java/br/com/senai/propetservice/config/OpenApiConfig.java
@@ -2,6 +2,7 @@ package br.com.senai.propetservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ public class OpenApiConfig {
         return new OpenAPI().info(
                 new Info()
                         .title("ProPet Service API")
+                        .description("Backend of ProPet System")
         );
     }
 }
