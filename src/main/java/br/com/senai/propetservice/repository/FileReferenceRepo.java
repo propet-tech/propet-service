22f55930c15611cb2a4a827ff4399618abc34a6f
deividrx <galdinodeivid@gmail.com>
2023-06-19 09:34:12 -0300

    refactor: repositories

--- src/main/java/br/com/senai/propetservice/repository/FileReferenceRepo.java
+++ src/main/java/br/com/senai/propetservice/repository/FileReferenceRepo.java
@@ -1,8 +1,10 @@
 package br.com.senai.propetservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.senai.propetservice.models.FileReference;
 
+@Repository
 public interface FileReferenceRepo extends JpaRepository<FileReference, Long> {
 }
--- src/main/java/br/com/senai/propetservice/repository/PetShopServiceRepo.java
+++ src/main/java/br/com/senai/propetservice/repository/PetShopServiceRepo.java
@@ -1,27 +1,11 @@
 package br.com.senai.propetservice.repository;
 
-import java.util.UUID;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.senai.propetservice.models.PetShopService;
-import br.com.senai.propetservice.models.enums.OrderStatus;
+import br.com.senai.propetservice.repository.search.SearchRepository;
 
 @Repository
-public interface PetShopServiceRepo extends JpaRepository<PetShopService, Long> {
-
-    // @Query("SELECT ps FROM PetShopService as ps WHERE ps.active = true")
-    // Page<PetShopService> findAllActive(Pageable pageable);
-    //
-    // @Modifying
-    // @Query("UPDATE PetShopService SET status = :status WHERE id = :id")
-    // void changeServiceStatus(Long id, OrderStatus status);
-    //
-    // @Query("SELECT ps FROM PetShopService as ps WHERE ps.user.id = :id")
-    // Page<PetShopService> findAllByUser(Pageable pageable, UUID id);
+public interface PetShopServiceRepo extends JpaRepository<PetShopService, Long>, SearchRepository<PetShopService> {
 }
